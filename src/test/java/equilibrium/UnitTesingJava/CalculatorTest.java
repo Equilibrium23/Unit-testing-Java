ff029e6292857b2d7ec1c190449cf7fdd110237c
Equilibrium <58709131+Equilibrium23@users.noreply.github.com>
2021-02-17 16:09:21 +0100

    Test simple calculator method (add)

--- src/test/java/equilibrium/UnitTesingJava/CalculatorTest.java
+++ src/test/java/equilibrium/UnitTesingJava/CalculatorTest.java
@@ -1,13 +1,22 @@
 package equilibrium.UnitTesingJava;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
 class CalculatorTest
 {
     @Test
-    public void test()
+    public void testCalculatorAdd()
     {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 5;
+        int expectedResult = 10;
 
+        int result = calculator.add(a,b);
+
+        Assertions.assertEquals(expectedResult,result);
     }
+
 }
