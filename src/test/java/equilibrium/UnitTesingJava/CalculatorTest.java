ee3453e0cdc2ec7fa1eb24aaa06e7fab546f0b02
Equilibrium <58709131+Equilibrium23@users.noreply.github.com>
2021-02-18 11:17:54 +0100

    Add nested classes to group tests and add @DisplayNames to make result more visible.

--- src/test/java/equilibrium/UnitTesingJava/CalculatorTest.java
+++ src/test/java/equilibrium/UnitTesingJava/CalculatorTest.java
@@ -1,22 +1,75 @@
 package equilibrium.UnitTesingJava;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CalculatorTest
 {
-    @Test
-    public void testCalculatorAdd()
+    private Calculator calculator = new Calculator();
+
+    @DisplayName("Add Tests")
+    @Nested
+    class AddTests
     {
-        Calculator calculator = new Calculator();
-        int a = 5;
-        int b = 5;
-        int expectedResult = 10;
+        @DisplayName("5 + 5 = 10")
+        @Test
+        public void should_add_5_to_5_and_return_10()
+        {
+            int a = 5;
+            int b = 5;
+            int expectedResult = 10;
 
-        int result = calculator.add(a,b);
+            int result = calculator.add(a,b);
 
-        Assertions.assertEquals(expectedResult,result);
+            Assertions.assertEquals(expectedResult,result);
+        }
+
+        @DisplayName("2 + 5 = 7")
+        @Test
+        public void should_add_2_to_5_and_return_7()
+        {
+            int a = 2;
+            int b = 5;
+            int expectedResult = 7;
+
+            int result = calculator.add(a,b);
+
+            Assertions.assertEquals(expectedResult,result);
+        }
     }
+
+    @DisplayName("Multiply Tests")
+    @Nested
+    class MultiplyTests
+    {
+        @DisplayName("5 * 5 = 25")
+        @Test
+        public void should_multiply_5_with_5_and_return_25()
+        {
+            int a = 5;
+            int b = 5;
+            int expectedResult = 25;
+
+            int result = calculator.multiply(a,b);
+
+            Assertions.assertEquals(expectedResult,result);
+        }
+
+        @DisplayName("2 * 7 = 14")
+        @Test
+        public void should_multiply_2_with_7_and_return_14()
+        {
+            int a = 2;
+            int b = 7;
+            int expectedResult = 14;
+
+            int result = calculator.multiply(a,b);
+
+            Assertions.assertEquals(expectedResult,result);
+        }
+    }
+
+
+
 
 }
